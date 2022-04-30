93c3e35de6269bc4fdc3af06aefc4f6ccf099805
PeterJ504 <peterjalderton@gmail.com>
2022-04-30 17:49:39 +0800

    2.6 Change remaining routes

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,7 +1,11 @@
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../features/account/account_screen.dart';
+import '../features/orders_list/orders_list_screen.dart';
 import '../features/products_list/products_list_screen.dart';
 
 final goRouter = GoRouter(
@@ -16,9 +20,36 @@ final goRouter = GoRouter(
           path: 'cart',
           // Use builder for default or pageBuilder to customize.
           pageBuilder: (context, state) => MaterialPage(
-              key: state.pageKey,
-              child: ShoppingCartScreen(),
-              fullscreenDialog: true),
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: ShoppingCartScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'orders',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: OrdersListScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'account',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: AccountScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'signIn',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const EmailPasswordSignInScreen(
+              formType: EmailPasswordSignInFormType.signIn,
+            ),
+          ),
         ),
       ],
     ),
