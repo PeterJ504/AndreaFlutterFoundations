4c2ec80b141f3beb3baaa6d56c2c92c629f82691
PeterJ504 <peterjalderton@gmail.com>
2022-05-18 12:05:58 +0800

    5.20 Bugfix _ filtering state updates

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -10,8 +10,8 @@ class FakeAuthRepository {
   AppUser? get currentUser => _authState.value;
 
   Future<void> signInWithEmailAndPassword(String email, String password) async {
-    await Future.delayed(const Duration(seconds: 3));
-    throw Exception('Connection failed');
+    await Future.delayed(const Duration(seconds: 2));
+    // throw Exception('Connection failed');
     if (currentUser == null) {
       _createNewUser(email);
     }
