fb0f01bc3266bcd8245e39175fca2f5c21cb2430
PeterJ504 <peterjalderton@gmail.com>
2022-05-19 00:23:50 +0800

    6.4 Write first unit test

---
+++ test/src/features/products/data/fake_products_repository_test.dart
@@ -0,0 +1,19 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'Should return global products list',
+    () async {
+      //arrange
+      //act
+      final productsRepository = FakeProductsRepository();
+      //assert
+      expect(
+        productsRepository.getProductsList(),
+        kTestProducts,
+      );
+    },
+  );
+}
