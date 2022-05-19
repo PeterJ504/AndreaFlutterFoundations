67abc9a597b10a7aa65be68867a52ed6d103c04b
PeterJ504 <peterjalderton@gmail.com>
2022-05-19 21:10:00 +0800

    6.7 Testing streams and futures

--- test/src/features/products/data/fake_products_repository_test.dart
+++ test/src/features/products/data/fake_products_repository_test.dart
@@ -3,43 +3,93 @@ import 'package:ecommerce_app/src/features/products/data/fake_products_repositor
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test(
-    'Should return global products list',
-    () async {
-      //arrange
-      //act
-      final productsRepository = FakeProductsRepository();
-      //assert
-      expect(
-        productsRepository.getProductsList(),
-        kTestProducts,
-      );
-    },
-  );
-  test(
-    'Should return first item in the global products list',
-    () async {
-      //arrange
-      //act
-      final productsRepository = FakeProductsRepository();
-      //assert
-      expect(
-        productsRepository.getProduct('1'),
-        kTestProducts[0],
-      );
-    },
-  );
-  test(
-    'Should return null when invalid item is requested',
-    () async {
-      //arrange
-      //act
-      final productsRepository = FakeProductsRepository();
-      //assert
-      expect(
-        productsRepository.getProduct('100'),
-        null,
-      );
-    },
-  );
+  group('Repository tests - ', () {
+    final productsRepository = FakeProductsRepository();
+
+    test(
+      'Should return global products list',
+      () async {
+        //arrange
+        //act
+        var result = productsRepository.getProductsList();
+        //assert
+        expect(result, kTestProducts);
+      },
+    );
+    test(
+      'Should return first item in the global products list',
+      () async {
+        //arrange
+        // final productsRepository = FakeProductsRepository();
+        //act
+        var result = productsRepository.getProduct('1');
+        //assert
+        expect(
+          result,
+          kTestProducts[0],
+        );
+      },
+    );
+  });
+
+  group('Repository tests for streams and futures - ', () {
+    final productsRepository = FakeProductsRepository();
+
+    test(
+      'Should return null when invalid item is requested',
+      () async {
+        //arrange
+        // final productsRepository = FakeProductsRepository();
+        //act
+        var result = productsRepository.getProduct('100');
+        //assert
+        expect(result, null);
+      },
+    );
+    test(
+      'Should return global list of products (future)',
+      () async {
+        //arrange
+        // final productsRepository = FakeProductsRepository();
+        //act
+        var result = productsRepository.getProductsList();
+        //assert
+        expect(result, kTestProducts);
+      },
+    );
+    test(
+      'Should emit global list of products (stream)',
+      () async {
+        //arrange
+        // final productsRepository = FakeProductsRepository();
+        //act
+        var result = productsRepository.watchProductsList();
+        //assert
+        expect(result, emits(kTestProducts));
+      },
+    );
+
+    test(
+      'Should emit first item (stream)',
+      () async {
+        //arrange
+        final productsRepository = FakeProductsRepository();
+        //act
+        var result = productsRepository.watchProduct('1');
+        //assert
+        expect(result, emits(kTestProducts[0]));
+      },
+    );
+    test(
+      'Should emit null for invalid item (stream)',
+      () async {
+        //arrange
+        // final productsRepository = FakeProductsRepository();
+        //act
+        var result = productsRepository.watchProduct('100');
+        //assert
+        expect(result, emits(null));
+      },
+    );
+  });
 }
